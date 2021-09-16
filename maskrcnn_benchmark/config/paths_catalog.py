1b35f8c8f7d1c637ddffed86c4c3181f88e6b07f
SikandarBakht <50270831+SikandarBakht@users.noreply.github.com>
2021-09-16 19:39:39 +0500

    Update paths_catalog.py

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -113,7 +113,7 @@ class DatasetCatalog(object):
         },
         "VG_stanford_filtered_with_attribute": {
             "img_dir": "vg/VG_100K",
-            "roidb_file": "/content/drive/MyDrive/SGG/VG-SGG-with-attri.h5",
+            "roidb_file": "vg/VG-SGG-with-attri.h5",
             "dict_file": "vg/VG-SGG-dicts-with-attri.json",
             "image_file": "vg/image_data.json",
         },
